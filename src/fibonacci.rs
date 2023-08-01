55888f2b36ba6b0a8907e5b3305ff986c538a0cf
nullity00 <reshmashaik3011@gmail.com>
2023-08-01 14:56:26 +0530

    fibo update

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -1,5 +1,5 @@
-use halo2_proofs::{circuit::*, plonk::*, poly::Rotation, arithmetic::Field};
 use std::marker::PhantomData;
+use halo2_proofs::{arithmetic::Field, circuit::*, plonk::*, poly::Rotation};
 
 #[derive(Clone, Copy, Debug)]
 struct FibonacciConfig {
@@ -7,94 +7,135 @@ struct FibonacciConfig {
   pub col_b : Column<Advice>,
   pub col_c : Column<Advice>,
   pub selector : Selector,
-  pub instance : Column<Instance>
+  pub instance : Column<Instance>,
 }
 
-#[derive(Clone, Debug)]
+#[derive(Debug, Clone)]
 struct FibonacciChip<F: Field> {
-  config : FibonacciConfig,
-  _marker : PhantomData<F>
+  config: FibonacciConfig,
+  _marker: PhantomData<F>,
 }
 
 impl<F: Field> FibonacciChip<F> {
-    pub fn construct(config: FibonacciConfig) -> Self {
-        Self {
-            config,
-            _marker: PhantomData,
-        }
+  pub fn construct(config: FibonacciConfig) -> Self {
+    Self {
+      config,
+      _marker: PhantomData,
     }
+  }
 
-    pub fn configure(meta: &mut ConstraintSystem<F>) -> FibonacciConfig{
-      let col_a = meta.advice_column();
-      let col_b = meta.advice_column();
-      let col_c = meta.advice_column();
-      let selector = meta.selector();
-      let instance = meta.instance_column();
+  pub fn configure(meta: &mut ConstraintSystem<F>) -> FibonacciConfig {
+    let col_a = meta.advice_column();
+    let col_b = meta.advice_column();
+    let col_c = meta.advice_column();
+    let selector = meta.selector();
+    let instance = meta.instance_column();
 
-      meta.enable_equality(col_a);
-      meta.enable_equality(col_b);
-      meta.enable_equality(col_c);
-      meta.enable_equality(instance);
+    meta.enable_equality(col_a);
+    meta.enable_equality(col_b);
+    meta.enable_equality(col_c);
+    meta.enable_equality(instance);
 
-      meta.create_gate("add", |meta|{
-
+    meta.create_gate("add", |meta| {
         let s = meta.query_selector(selector);
         let a = meta.query_advice(col_a, Rotation::cur());
         let b = meta.query_advice(col_b, Rotation::cur());
         let c = meta.query_advice(col_c, Rotation::cur());
-        vec![s * ( a + b - c)]
-      });
+        vec![s * (a + b - c)]
+  });
 
-      FibonacciConfig { col_a, col_b, col_c, selector, instance}
+  FibonacciConfig {
+      col_a,
+      col_b,
+      col_c,
+      selector,
+      instance,
+  }
+  }
+
+  pub fn assign_first_row(
+        &self,
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(AssignedCell<F, F>, AssignedCell<F, F>, AssignedCell<F, F>), Error> {
+        layouter.assign_region(
+            || "first row",
+            |mut region| {
+                self.config.selector.enable(&mut region, 0)?;
+
+                let a_cell = region.assign_advice_from_instance(
+                    || "f(0)",
+                    self.config.instance,
+                    0,
+                    self.config.col_a,
+                    0)?;
+
+                let b_cell = region.assign_advice_from_instance(
+                    || "f(1)",
+                    self.config.instance,
+                    1,
+                    self.config.col_b,
+                    0)?;
+
+                let c_cell = region.assign_advice(
+                    || "a + b",
+                    self.config.col_c,
+                    0,
+                    || a_cell.value().copied() + b_cell.value(),
+                )?;
+
+                Ok((a_cell, b_cell, c_cell))
+            },
+        )
     }
 
-    pub fn assign_row(&self, mut layouter: impl Layouter<F>, nrows: usize)-> Result<AssignedCell<F, F>, Error>{
-      layouter.assign_region(|| "fibo table", |mut region|{
-        self.config.selector.enable(&mut region, 0)?;
-        self.config.selector.enable(&mut region, 1)?;
+    pub fn assign_row(
+        &self,
+        mut layouter: impl Layouter<F>,
+        prev_b: &AssignedCell<F, F>,
+        prev_c: &AssignedCell<F, F>,
+        row_num: &String,
+    ) -> Result<AssignedCell<F, F>, Error> {
+        layouter.assign_region(
+            || row_num,
+            |mut region| {
+                self.config.selector.enable(&mut region, 0)?;
 
-        let mut a_cell = region.assign_advice(
-          || "a",
-          self.config.col_a,
-          0,
-          || Value::known(F::ZERO),
-        )?;
+                prev_b.copy_advice(
+                    || "a",
+                    &mut region,
+                    self.config.col_a,
+                    0,
+                )?;
+                prev_c.copy_advice(
+                    || "b",
+                    &mut region,
+                    self.config.col_b,
+                    0,
+                )?;
 
-        let mut b_cell = region.assign_advice(
-          || "b",
-          self.config.col_b,
-          0,
-          || Value::known(F::ONE),
-        )?;
+                let c_cell = region.assign_advice(
+                    || "c",
+                    self.config.col_c,
+                    0,
+                    || prev_b.value().copied() + prev_c.value(),
+                )?;
 
-        for _i in 2..nrows {
-          let c_cell = region.assign_advice(
-            || "c",
-            self.config.col_c,
-            0,
-            || a_cell.value().copied() + b_cell.value(),
-          )?;
-          a_cell = b_cell;
-          b_cell = c_cell;
-        }
-
-        Ok(b_cell)
-      },)
+                Ok(c_cell)
+            },
+        )
     }
 
     pub fn expose_public(
-      &self,
-      mut layouter: impl Layouter<F>,
-      cell: AssignedCell<F, F>,
-      row: usize,
+        &self,
+        mut layouter: impl Layouter<F>,
+        cell: &AssignedCell<F, F>,
+        row: usize,
     ) -> Result<(), Error> {
         layouter.constrain_instance(cell.cell(), self.config.instance, row)
     }
 }
 
-
 #[derive(Default)]
-
 struct FiboCircuit<F>(PhantomData<F>);
 
 impl<F: Field> Circuit<F> for FiboCircuit<F> {
@@ -114,13 +155,21 @@ impl<F: Field> Circuit<F> for FiboCircuit<F> {
         config: Self::Config,
         mut layouter: impl Layouter<F>,
     ) -> Result<(), Error> {
-      let chip  = FibonacciChip::construct(config);
+        let chip = FibonacciChip::construct(config);
 
-      let out_cell = chip.assign_row(layouter.namespace(|| "entire table"), 10)?;
+        let (_, mut prev_b, mut prev_c) =
+            chip.assign_first_row(layouter.namespace(|| "first row"))?;
 
-      chip.expose_public(layouter.namespace(|| "out"), out_cell, 2)?;
+        for i in 3..10 {
+            let c_cell = chip.assign_row(layouter.namespace(|| "next row"), &prev_b, &prev_c, &i.to_string())?;
+            prev_b = prev_c;
+            prev_c = c_cell;
+        }
 
-      Ok(())    }
+        chip.expose_public(layouter.namespace(|| "out"), &prev_c, 2)?;
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -147,8 +196,6 @@ mod tests {
 
         public_input[2] += Fp::one();
         let _prover = MockProver::run(k, &circuit, vec![public_input]).unwrap();
-        // uncomment the following line and the assert will fail
-        // _prover.assert_satisfied();
     }
 
     #[cfg(feature = "dev-graph")]
@@ -160,7 +207,7 @@ mod tests {
         root.fill(&WHITE).unwrap();
         let root = root.titled("Fib 1 Layout", ("sans-serif", 60)).unwrap();
 
-        let circuit = MyCircuit::<Fp>(PhantomData);
+        let circuit = FiboCircuit::<Fp>(PhantomData);
         halo2_proofs::dev::CircuitLayout::default()
             .render(4, &circuit, &root)
             .unwrap();
